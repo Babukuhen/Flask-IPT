6f6466cc101b20eda0159a69f2d40dddc3b695fb
Julius Vincent D. Llavan <juliusllavan@gmail.com>
2024-05-23 01:33:01 +0800

    'Data Fetch' Function & 'Get Employee' Function ADDED

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
@@ -15,6 +15,20 @@ mysql = MySQL(app)
 @app.route("/")
 def page_display():
     return "<p>Hello, World!</p>"
+
+
+def data_fetch(query):
+    cur = mysql.connection.cursor()
+    cur.execute(query)
+    data = cur.fetchall()
+    cur.close()
+    return data
+
+
+@app.route("/employee", methods=["GET"])
+def get_employee():
+    data = data_fetch("""SELECT * FROM employee""")
+    return make_response(jsonify(data), 200)
 
 
 if __name__ == "__main__":
