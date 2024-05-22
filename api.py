c0b6e6b21b175c517c7b1d8f38333d62cf2ad3b6
Julius Vincent D. Llavan <juliusllavan@gmail.com>
2024-05-23 01:43:06 +0800

    'Get Employee by Employee ID' Function ADDED

--- api.py
+++ api.py
@@ -27,8 +27,37 @@ def data_fetch(query):
 
 @app.route("/employee", methods=["GET"])
 def get_employee():
+    # Get All Employee Details
     data = data_fetch("""SELECT * FROM employee""")
     return make_response(jsonify(data), 200)
+
+
+@app.route("/employee/<int:id>", methods=["GET"])
+def get_employee_by_id(id):
+    # Get Employee by Employee ID
+    data = data_fetch(f"""SELECT * FROM employee WHERE employee_id = {id}""")
+    return make_response(jsonify(data), 200)
+
+
+@app.route("/employee", methods=["POST"])
+def add_employee():
+    pass
+
+
+@app.route("/employee/<int:id>", methods=["PUT"])                       # FIX
+def update_employee(id):
+    pass
+
+
+@app.route("/employee/<int:id>", methods=["DELETE"])
+def delete_employee(id):
+    pass
+
+
+@app.route("/employee/format", methods=["GET"])
+def get_params():
+    pass
+
 
 
 if __name__ == "__main__":
